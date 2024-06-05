37c240f5990a95a70ab00c59b395ae0ae2709b6a
Иван <76448025+IvanErshov@users.noreply.github.com>
2024-06-05 22:16:58 +0300

    Update Neural.py

--- Neural.py
+++ Neural.py
@@ -8,6 +8,16 @@ from sklearn.model_selection import train_test_split
 
  from tqdm import tqdm
 
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+y_true = [3, -0.5, 2, 7]
+y_pred = [2.5, 0.0, 2, 8]
+
+mae = mean_absolute_error(y_true, y_pred)
+mse = mean_squared_error(y_true, y_pred)
+
+print("MAE:", mae)
+print("MSE:", mse)
 
 
 
